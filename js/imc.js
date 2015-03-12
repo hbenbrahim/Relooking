56d2f753b069259157abe180c9208e38db418d42
hbenbrahim <hbenbrahim@webuildmedia.com>
2015-03-12 16:04:59 +0000

    Little opitimisation of imc script.

--- js/imc.js
+++ js/imc.js
@@ -3,6 +3,5 @@ function compute() {
 		hauteur = document.getElementById('Height');
 	var imcAff = document.getElementById('IMC');
 	imc = poids.value /((hauteur.value/100)*(hauteur.value/100)) ;
-	var imc2 = Math.round(imc*100)/100;
-	imcAff.value = imc2;
+	imcAff.value = Math.round(imc*100)/100;;
 } 
