8e4a1186c1791dbd5edbde97a9c73012882c84ba
hbenbrahim <hbenbrahim@webuildmedia.com>
2015-03-19 12:40:29 +0000

    Apparently fixed the mobile placing bug and beautified the code

--- js/imc.js
+++ js/imc.js
@@ -1,7 +1,7 @@
 function compute() {
-	var poids = document.getElementById('Weight'),
-		hauteur = document.getElementById('Height');
-	var imcAff = document.getElementById('IMC');
-	imc = poids.value /((hauteur.value/100)*(hauteur.value/100)) ;
-	imcAff.value = Math.round(imc*100)/100;;
-} 
+  var poids = document.getElementById('Weight'),
+    hauteur = document.getElementById('Height');
+  var imcAff = document.getElementById('IMC');
+  imc = poids.value / ((hauteur.value / 100) * (hauteur.value / 100));
+  imcAff.value = Math.round(imc * 100) / 100;;
+}
--- mail.php
+++ mail.php
@@ -7,14 +7,14 @@ error_reporting(E_ALL);
 */
 // destinataire est votre adresse mail. Pour envoyer à plusieurs à la fois, séparez-les par une virgule
 $destinataire = 'contact@relookingkenitra.com';
- 
+
 // copie ? (envoie une copie au visiteur)
 $copie = 'oui'; // 'oui' ou 'non'
- 
+
 // Messages de confirmation du mail
 $message_envoye = "Votre message nous est bien parvenu !";
 $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
- 
+
 /*
 	********************************************************************************************
 	FIN DE LA CONFIGURATION
@@ -31,11 +31,11 @@ $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
 		{
 			$text = stripslashes($text);
 		}
- 
+
 		$text = nl2br($text);
 		return $text;
 	};
- 
+
 	/*
 	 * Cette fonction sert à vérifier la syntaxe d'un email
 	 */
@@ -44,20 +44,20 @@ $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
 		$value = preg_match('/^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!\.)){0,61}[a-zA-Z0-9_-]?\.)+[a-zA-Z0-9_](?:[a-zA-Z0-9_\-](?!$)){0,61}[a-zA-Z0-9_]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/', $email);
 		return (($value === 0) || ($value === false)) ? false : true;
 	}
- 
+
 	// formulaire envoyé, on récupère tous les champs.
 	$nom     = (isset($_POST['InputName']))     ? Rec($_POST['InputName'])     : '';
 	$prenom  = (isset($_POST['InputLast']))     ? Rec($_POST['InputLast'])     : '';
 	$email   = (isset($_POST['InputEmail']))    ? Rec($_POST['InputEmail'])    : '';
 	$objet   = (isset($_POST['InputNumber']))   ? Rec($_POST['InputNumber'])   : '';
 	$message = (isset($_POST['InputMessage']))  ? Rec($_POST['InputMessage'])  : '';
- 
+
 	// les 4 variables sont remplies, on génère puis envoie le mail
 	$headers  = 'MIME-Version: 1.0' . "\r\n";
 	$headers .= 'From:'.$nom.' '.$prenom. ' <'.$email.'>' . "\r\n" .
 					'Reply-To:'.$email. "\r\n" .
 					'X-Mailer:PHP/'.phpversion();
- 
+
 	// envoyer une copie au visiteur ?
 	if ($copie == 'non')
 	{
@@ -67,7 +67,7 @@ $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
 	{
 		$cible = $destinataire;
 	};
- 
+
 	// Remplacement de certains caractères spéciaux
 	$message = str_replace("&#039;","'",$message);
 	$message = str_replace("&#8217;","'",$message);
@@ -77,7 +77,7 @@ $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
 	$message = str_replace("&lt;","<",$message);
 	$message = str_replace("&gt;",">",$message);
 	$message = str_replace("&amp;","&",$message);
- 
+
 	// Envoi du mail
 	if (mail($cible, $objet, $message, $headers))
 	{
@@ -87,5 +87,5 @@ $message_non_envoye = "L'envoi du mail a échoué, veuillez réessayer SVP.";
 	{
 		echo '<p>'.$message_non_envoye.'</p>'."\n";
 	};
-		
+
 ?>
